8da0c09a2ef103dd25bffd9bc65d5e3f29ea567e
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-10-13 19:57:33 +0000

    Creating branch for BioC 3.2 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_2/madman/Rpacks/inveRsion@109591 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/inversionListClass.R
@@ -0,0 +1,265 @@
+##Define inversion##
+setClass(Class="inversion",
+	     representation=representation(classification="vector",leftBP="vector",rightBP="vector",bic="vector",intLeftBP="vector", intRightBP="vector",invFreq="numeric",RR="list"))
+
+##Define inversionList##
+setClass(Class="inversionList",
+	     representation=representation(results="list"))
+
+
+##Show inversionList#  
+setMethod("show","inversionList",
+    function(object)
+    {
+       cat("-Showing object of class: inversionList- \n\n")
+
+       mat<-c()
+       if (length(object@results)==0)
+       { 
+             cat("Void: No inversions computed \n")
+       }else{
+         for(nroi in 1:length(object@results))
+         {
+             if(class(object@results[[nroi]])!="inversion")
+             {
+                cat("List entry is not of class inversion")
+                mm<-rep(NA,6)
+             }else{
+                mm<-c(object@results[[nroi]]@intLeftBP,object@results[[nroi]]@intRightBP,max(object@results[[nroi]]@bic),object@results[[nroi]]@invFreq,length(object@results[[nroi]]@bic))
+                names(mm)<-c()
+             }
+              
+             mat<-rbind(mat,mm)  
+         }    
+         
+         rownames(mat)<-1:length(object@results)
+         colnames(mat)<-c("LBPmin","LBPmax","RBPmin","RBPmax","MaxBic","invFreq","ModelNum")
+         print(mat)   
+       }
+   }
+)   
+
+##plot inversionList##
+setMethod(f="plot",signature=c(x="inversionList"),
+    definition=function(x,y,wROI,...)
+	{
+       if(missing(wROI))
+       {
+          a<-c()
+          for(nroi in 1:length(x@results))
+          {
+            mm<-cbind(x@results[[nroi]]@leftBP,x@results[[nroi]]@rightBP,x@results[[nroi]]@bic)
+            names(mm)<-c()
+            a<-rbind(a,mm)  
+          }    
+
+         lab<-"BIC"
+         nr<-NROW(a)
+         plot(c(min(a[1:nr,1]), max(a[1:nr,2])),c(min(a[1:nr,3]),max(a[1:nr,3])),ylab=lab, xlab="Segments Tested",pch="",...)
+ 
+         for(ss in 1:nr)
+         {
+            lines(c(a[ss,1],a[ss,2]),c(a[ss,3],a[ss,3]))
+          }  
+        }else{
+        
+         hist(x@results[[wROI]]@classification,xlab="average responsibilities of subjects across models",main=c("Histogram of subject responsibilities for ROI: ",
+         paste(round(x@results[[wROI]]@intLeftBP[1],3),round(x@results[[wROI]]@intRightBP[2],3),sep="-")),...)
+      
+        }
+    
+    }
+)
+
+
+#generic functions
+setGeneric("getClassif",function(object,thBic,wROI,bin,geno,id,nmod){standardGeneric("getClassif")})
+
+
+setMethod("getClassif","inversionList",         
+         function(object,thBic,wROI,bin,geno,id,nmod)
+         {
+
+            if(missing(wROI))
+             wROI<-1
+            
+            if(missing(thBic))
+             thBic<-0
+
+            if(missing(bin))
+             bin<-TRUE
+
+            if(missing(geno))
+             geno<-TRUE
+
+            nchr<-length(object@results[[wROI]]@RR[[1]])
+            
+            if(missing(id))
+            {
+             id<-paste("sub",1:(nchr/2),sep="")
+            }
+            
+         
+          
+            RR<-object@results[[wROI]]@RR
+            bic<-object@results[[wROI]]@bic
+
+            o1<-order(-bic)
+
+            RR<-RR[o1]
+            bic<-bic[o1]
+
+            sb<-bic>thBic
+
+            ii<-(1:length(RR))[sb]
+
+            lni<-length(ii)
+        
+            if(!missing(nmod))
+               ii<-ii[1:min(nmod,lni)]            
+
+            nbic<-bic[ii]/mean(bic[ii])
+
+            #for each subject see the average classification given across models
+            r<-sapply(1:length(RR[[1]]), function(y) mean(nbic*sapply(ii,function(x) RR[[x]][y])))
+
+           
+            message("\n getting mean classification across ", length(ii), " models \n")
+
+           
+            if(geno)
+            {
+            
+              if(bin)
+                 r<-as.numeric(r>0.5)
+ 
+              even<-2*(1:(length(r)/2))
+              odd<-even-1
+              homInv<-r[even]*r[odd]
+              het<-(1-r[even])*r[odd]+r[even]*(1-r[odd])
+              hom<-(1-r[even])*(1-r[odd])
+              id<-id
+              ans<-data.frame(id=id, hom=hom, het=het, homInv=homInv) 
+              ans
+            
+            }else{
+            
+              if(bin)
+              { 
+                ans<-as.numeric(r>0.5)
+                names(ans)<-rep(unlist(id),each=2)
+                ans
+              }else{
+                ans<-r
+                names(ans)<-rep(unlist(id),each=2)
+                ans
+              }
+            }
+         }
+
+)                
+
+
+##Define Class##
+setClass(Class="accuracy", representation=representation(out="matrix"))
+
+
+##Class Contructor which is also a method for inversionList##
+setGeneric("accBic",function(object,mem,classFile,nsub,npoints,geno,wROI){standardGeneric("accBic")})
+
+ 
+setMethod("accBic","inversionList", 
+         function(object,mem,classFile,nsub,npoints,geno,wROI)
+         {
+
+            if(missing(mem))
+            { 
+              if(missing(classFile)) 
+                stop("\n please provide either vector or file with the inversion staus for the chromosomes of all subjects")
+
+              mem<-read.table(file=classFile,header=FALSE)
+              mem<-unlist(mem)
+            }
+
+            if(missing(nsub))
+              stop("\n plase provide number of subjects in nsub")           
+              
+            if(missing(geno))
+              geno<-FALSE  
+
+            if(missing(wROI))
+              wROI<-1
+              
+            
+            nchr<-1:(2*nsub)
+            ev<-2*(1:nsub)
+            odd<-ev-1
+            
+            #genotype classification
+            popGeno<-odd%in%nchr[mem]+ev%in%nchr[mem]
+
+            #retrive classification for each probe sequence (fixed window model) from object
+            RR<-object@results[[wROI]]@RR
+            
+            ac<-c()
+            prob<-c()
+ 
+            bicInt<- seq(0,max(object@results[[1]]@bic,na.rm=TRUE),length.out=npoints)
+            bicInt<-bicInt[-length(bicInt)]
+             message("\n computing accuracy for ", npoints," bic thresholds: ")
+
+            for(thBic in bicInt)
+            {
+                #select models with bic>thBic
+                cat(".")
+               
+                sb<-object@results[[wROI]]@bic>thBic 
+                sb[is.na(sb)]<-FALSE
+                ii<-(1:length(RR))[sb]
+                
+                #for each subject see the average classification given across models 
+                r<-sapply(1:length(RR[[1]]), function(y) mean(sapply(ii,function(x) RR[[x]][y]<0.5)))
+                
+                if(geno)
+                {
+                  #binirize average classification into a final classification for each subject 
+                  PHits<-odd%in%nchr[r>0.5]+ev%in%nchr[r>0.5] 
+                
+                  #compare with real classification
+                  ac<-c(ac, mean(popGeno==PHits) )
+                }else{
+                  PHits<-nchr%in%nchr[mem]
+                  popHaplo<-nchr%in%nchr[r>0.5]
+                  ac<-c(ac,mean(popHaplo==PHits))
+                
+                }   
+                  
+                prob<-c(prob,mean(r>0.5))
+
+            }
+            
+            
+           ans<-cbind(bicInt,prob,ac) 
+    
+           new("accuracy", out=ans)   
+
+
+    }
+)
+
+
+##plot accuracy##
+setMethod(f="plot",signature=c(x="accuracy"),
+    definition=function(x,y,w="a",...)
+	{
+        wplot<-charmatch(w,c("f", "a"))
+ 
+        lab<-switch(wplot,"Inversion Frequency","accuracy")
+
+        plot(x@out[,1],x@out[,1+wplot],t="l",xlab="BIC",ylab=lab,...)
+        points(x@out[,1],x@out[,1+wplot])
+
+    }
+)
+
+         
---
+++ src/writeGenoDat.c
@@ -0,0 +1,151 @@
+/* Funtion to process genotype data from snpMatrix package, stored as raw R data
+
+compile with: R CMD SHLIB ./writeGenoDat.c
+
+test on R:
+
+lev <- as.raw(c(0,1,2,3))
+lev2<-as.raw(c(0,3,2,1))
+gg<-rbind(lev,lev,lev,lev,lev,lev2)
+
+ggin<-as.vector(gg)
+
+nprobes<-NCOL(gg)
+nsub<-NROW(gg)
+nlev<-length(lev)
+
+allesum<-TRUE
+caco<-c(0,0,1,1,1,2)
+levcaco<-c(0,1,2)
+numlevcaco<-length(levcaco)
+
+
+dyn.load("writeGenoDat.so")
+ans<-.C("writeGenoDat",as.integer(ggin),as.integer(nprobes),as.integer(nsub),as.integer(lev),as.integer(nlev),as.integer(allesum),as.integer(caco),as.integer(levcaco),as.integer(numlevcaco),outInv=as.double(rep(0,nprobes)),outAleleSum=as.double(rep(0,numlevcaco*nprobes)),outNoMissCount=as.double(rep(0,numlevcaco*nprobes)))
+ 
+*/
+
+#include "inversion.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void writeGenoDat(int *geno, int *numprobes, int *numsub, int *lev, int *numlev, int *sumallele, int *caco, int *levcaco, int *numlevcaco, double *outInv, double *outAleleSum, double *outNoMissCount)
+{
+   int i,j,l,k,dat,nomiss[*numlevcaco],sum[*numlevcaco];
+   double allele1sum,allele2sum,invallele;
+   FILE *fg;
+
+   fg = fopen("GenoDatTemp.txt","w");
+	
+   for(j=0; j< *numprobes; j++)
+   {
+      /*initialize sums for each probe*/
+      for(k=0; k<*numlevcaco; k++)
+      {
+         sum[k]=0;
+		 nomiss[k]=0;
+      }
+	
+	
+	  allele1sum=0.0;
+	  allele2sum=0.0;
+	  
+	  /*identify minor and mayor alleles*/
+	  for (i=0; i<*numsub; i++)
+	  {  
+		 if (geno[i+j*(*numsub)]==lev[1]) 
+		 { 
+		    allele1sum=allele1sum+1; 
+		 }
+		 if (geno[i+j*(*numsub)]==lev[3])
+		 {
+		     allele2sum=allele2sum+1;
+		  }	
+	  } 
+	  
+	  invallele=allele1sum-allele2sum;	
+	  /*run over subjects*/
+	  for (i=0; i<*numsub; i++)
+	  {  
+	
+	    
+    	 for (l=0; l<*numlev; l++)  /* run over numlevels to check which one the data point belongs to and then assign the corresponding integer*/
+	     { 
+           if (geno[i+j*(*numsub)]==lev[l])
+           {
+		     dat=l;
+			}
+         } 
+		 
+		 outInv[j]=0;
+		 if(invallele<0)
+		 { 
+		   outInv[j]=1;
+		   
+		   switch(dat){
+		   case 1: 
+		     dat=3;
+			 break;
+		   case 3:
+		     dat=1;
+			 break;
+		   }
+	     }
+		 
+		 
+		/*write GenoDat*/ 
+		if(dat<1)
+		 {
+		   fprintf(fg,"NA ");	
+		 }
+		 else
+		 {
+	       fprintf(fg,"%d ", dat-1);	
+		 }
+		 
+		 /*compute Allele Sums by subject level*/
+		 if(*sumallele==1)
+         {
+		   for(k=0; k<*numlevcaco; k++)/* run over cacolevels to check which subject the data point belongs and add it's allele (dat-1) to the subject group*/
+		   {
+	         if(dat>0) /*discard missings encoded by 0*/
+		     { 
+						
+		       if (caco[i]==levcaco[k])
+			   {
+	             sum[k]=sum[k]+(dat-1);	/*sum alleles by level*/
+				 nomiss[k]=nomiss[k]+1; /*count not missing data*/
+			    }
+		      }
+		    }
+		  } 
+		
+		
+	   }/*end subject run*/
+	   
+		
+	   fprintf(fg,"\n"); /*brake line for geno data*/
+
+	   /*write AlleleSumDat and notMissDat*/
+	   if(*sumallele==1)
+	   {
+	     for(k=0; k<*numlevcaco; k++)
+	     {
+		    outAleleSum[k+j*(*numlevcaco)]=sum[k];
+			outNoMissCount[k+j*(*numlevcaco)]= nomiss[k];
+		  }	
+
+	   }
+	   
+	  
+    }
+
+   fclose(fg);  
+  
+   
+   return;
+
+}
+
+  
+ 
